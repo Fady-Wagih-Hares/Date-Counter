8c982ccc886736476f8c776c4917addef8fb9a78
fady alhares <fadyalhares@gmail,com>
2026-06-25 20:10:23 +0300

    version 2

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -4,36 +4,64 @@ import DateDisplay from "./DateDisplay";
 const Counter = () => {
   const [step, setStep] = useState(1);
   const [count, setCount] = useState(0);
-
+  // const [inputState, setInputState] = useState(1);
   // console.log(count);
-  const handleInc = () => {
-    setStep((step) => step + 1);
-    // console.log(step);
-  };
-  const handleDec = () => {
-    setStep((step) => step - 1);
-    // console.log(step);
-  };
+  // const handleInc = () => {
+  //   setStep((step) => step + 1);
+  // console.log(step);
+  // };
+  // const handleDec = () => {
+  //   setStep((step) => step - 1);
+  // console.log(step);
+  // };
   const handleCountInc = () => {
     setCount((count) => count + step);
   };
   const handleCountDec = () => {
     setCount((count) => count - step);
   };
+  const handleRest = () => {
+    setCount(1);
+    setStep(0);
+  };
   return (
     <div className="main">
-      <button onClick={() => handleDec()}>-</button>
-      <span>Steps:{`${step}`} </span>
-      <button onClick={() => handleInc()}>+</button>
+      <span>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={step}
+          onChange={(e) => setStep(+e.target.value)}
+        />
+        {step}
+      </span>
+
+      {/* <button onClick={() => handleDec()}>-</button>
+      <span>Steps:{`${step}`} </span> */}
+      {/* <button onClick={() => handleInc()}>+</button> */}
       <div className="count">
         <button onClick={() => handleCountDec()}>-</button>
-        <span>Count:{`${count}`} </span>
+        {/* <span>Count:{`${count}`} </span> */}
+
+        <input
+          type="text"
+          value={count}
+          onChange={(e) => setCount(+e.target.value)}
+        />
+
         <button onClick={() => handleCountInc()}>+</button>
       </div>
 
       <div>
         <DateDisplay count={count} />
       </div>
+
+      {count !== 1 || step !== 0 ? (
+        <div>
+          <button onClick={handleRest}>Rest</button>
+        </div>
+      ) : null}
     </div>
   );
 };
